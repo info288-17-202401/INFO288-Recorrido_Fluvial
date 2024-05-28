95e13a4e8d32976389e071f10990829bcf7f41be
Jorge Gonzalez <berna2022@gmail.com>
2024-05-28 16:12:03 -0400

    cambios de otra rama

--- server/main.py
+++ server/main.py
@@ -1,29 +1,48 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
+import json
+from bson import ObjectId
 
 app = Flask(__name__)
 CORS(app)
 
-# Conectar a MongoDB
 client = MongoClient('mongodb://localhost:27017/')
-db = client['test']
-collection = db['ubicacion']
+db = client['ubicaciones']
+ports_collection = db['puertos']
+routes_collection = db['rutas']
 
-@app.route('/ruta', methods=['POST'])
-def manejar_ubicacion():
-    datos = request.json
-    latitud = datos.get('latitud')
-    longitud = datos.get('longitud')
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return super().default(o)
 
-    if latitud is not None and longitud is not None:
-        # Insertar datos en MongoDB
-        collection.insert_one({'latitud': latitud, 'longitud': longitud})
-        
-        print(f'Ubicación recibida - Latitud: {latitud}, Longitud: {longitud}')
-        return 'Ubicación recibida correctamente', 200
+@app.route('/')
+def hello_world():
+    return jsonify(message="Hello, World!")
+
+@app.route('/location', methods=['POST'])
+def location():
+    data = request.json
+    print(f"Device location: {data}")
+    return jsonify(status="success", data=data)
+
+@app.route('/ports', methods=['GET'])
+def get_ports():
+    ports = list(ports_collection.find())
+    return JSONEncoder().encode(ports)
+
+@app.route('/route/<route_name>', methods=['GET'])
+def get_route(route_name):
+    route = routes_collection.find_one({"route_name": route_name}, {"_id": 0, "locations": 1})
+    if route and 'locations' in route:
+        return JSONEncoder().encode(route['locations'])
     else:
-        return 'Error: Datos de ubicación faltantes', 400
+        return jsonify({"error": "Route not found"}), 404
+
+
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0" , port= 5000, debug=True)
+    app.run(debug=True)
+
