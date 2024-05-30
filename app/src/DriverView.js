466142296a8a9a3e1c5dc3a677e8d34f65f7b79a
leoxz98 <leoxz98@gmail.com>
2024-05-30 16:46:43 -0400

    routes y otros

---
+++ app/src/DriverView.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DriverView = ({ mapWidth, mapHeight })=> {
+    return (
+        <div style={{ width: mapWidth, height: mapHeight }}>
+            aqui trabajan ustedes
+        </div>
+    );
+}
+
+export default DriverView;
